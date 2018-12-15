28360fe087c6d8368981838c109f43f48c452d4e
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 16:29:08 +0700

    Add files via upload

---
+++ Menghitung pemasukan dan pengeluaran.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int pemasukan, MAX=5000;
+
+cout<<"Program menentukan pemasukan lebih besar atau kecil dari pengeluaran"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"311810183"<<endl;
+cout<<"Masukan nilai pemasukan"<<endl;
+cin>>pemasukan;
+
+if (pemasukan > MAX)
+    cout<<"Pemasukan lebih besar dari pengeluaran:";
+    else
+        cout<<"Pemasukan lebih kecil dari pengeluaran:";
+    return 0;
+
+}
